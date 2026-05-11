912b1df78689b7539a5ff50cce78d1abac63306c
Alan Jeremy Jasso Vargas <alan.jasso@CONTL57GP6WNY7.local>
2026-05-10 20:54:37 -0600

    fix: ignore canvas and sharp bundling via experimental option

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['canvas', 'sharp'],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
